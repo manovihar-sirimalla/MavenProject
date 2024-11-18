85304f36b9586dfdbea71b20bf09097489bca9bd
manovihar-sirimalla <smv.manovihar@gmail.com>
2024-11-18 11:14:08 +0530

    First commit of Maven Project

---
+++ src/main/java/SEGroup/MavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package SEGroup.MavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is my First Maven Java project in Ecplise!!" );
+    }
+}
